a444d3987e2f4c7c25f6666086b1037c0d7551c3
Siphokazi Siko <sisiko022@student.wethinkcode.co.za>
2023-10-03 13:15:34 +0200

    added better visuals

--- Weather/src/main/java/vsuals.java
+++ Weather/src/main/java/vsuals.java
@@ -1,4 +1,4 @@
-import java.awt.BorderLayout;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
@@ -6,30 +6,65 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
+import javax.swing.*;
 
 public class vsuals extends JFrame{
     private static final long serialVersionUID = 1L;
     private JTextArea responseTextArea;
+    private JTextField locationTextField;
     public vsuals() {
         setTitle("WeatherAPI Example");
-        setSize(400, 300);
+        setSize(400, 200); // Adjusted the height for a cleaner look
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel contentPanel = new JPanel();
-        contentPanel.setLayout(new BorderLayout());
-        responseTextArea = new JTextArea();
-        contentPanel.add(responseTextArea, BorderLayout.CENTER);
+
+        JPanel contentPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5); // Add some padding
+
+        // Create a label for the location input
+        JLabel locationLabel = new JLabel("Enter Location:");
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        contentPanel.add(locationLabel, gbc);
+
+        // Create the location text field
+        locationTextField = new JTextField(20);
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        contentPanel.add(locationTextField, gbc);
+
+        // Create a label for the response
+        JLabel responseLabel = new JLabel("Weather Response:");
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        contentPanel.add(responseLabel, gbc);
+
+        // Create the response text area within a scroll pane
+        responseTextArea = new JTextArea(5, 20);
+        JScrollPane scrollPane = new JScrollPane(responseTextArea);
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        gbc.fill = GridBagConstraints.BOTH;
+        contentPanel.add(scrollPane, gbc);
+
+        // Create the fetch button
         JButton fetchButton = new JButton("Fetch Weather");
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.gridwidth = 2;
+        gbc.fill = GridBagConstraints.NONE;
+        contentPanel.add(fetchButton, gbc);
+
         fetchButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 fetchWeather();
             }
         });
-        contentPanel.add(fetchButton, BorderLayout.SOUTH);
+
         setContentPane(contentPanel);
+        pack(); // Pack the frame to fit the components nicely
+        setLocationRelativeTo(null); // Center the frame on the screen
         setVisible(true);
     }
     private void fetchWeather() {
